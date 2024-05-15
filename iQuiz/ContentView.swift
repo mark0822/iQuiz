c85644e59fa8d39d846aca8eac732e14e701ea12
mark0822 <mark20020822@gmail.com>
2024-05-15 15:57:04 -0700

    finished part 1

--- iQuiz/ContentView.swift
+++ iQuiz/ContentView.swift
@@ -7,15 +7,37 @@
 
 import SwiftUI
 
+struct Topic : Hashable{
+    let name: String
+    let description: String
+    let id = UUID()
+}
+
 struct ContentView: View {
+    let topics = [Topic(name: "Science!", description: "Because SCIENCE!"),Topic(name: "Mathematics", description: "Did you pass the third grade?"),Topic(name: "Marvel Super Heroes", description: "Avengers, Assemble!")]
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+            NavigationStack {
+                List(topics, id: \.self) { topic in
+                    NavigationLink{
+                        questionView()
+                    } label: {
+                        VStack(alignment: .leading){
+                            Label(topic.name, systemImage: "folder")
+                            Text(topic.description)
+                        }
+                    }
+                }
+                .toolbar {
+                    ToolbarItem(placement:
+                        .topBarTrailing) {
+                        Button("Settings") {
+                            print("Settings clicked")
+                        }
+                    }
+                }
+            }
         }
-        .padding()
     }
 }
 
---
+++ iQuiz/questionView.swift
@@ -0,0 +1,16 @@
+//
+//  questionView.swift
+//  iQuiz
+//
+//  Created by Mark on 5/15/24.
+//
+
+import Foundation
+import SwiftUI
+
+struct questionView: View {
+    var body: some View {
+        Text("questionView")
+    }
+}
+
